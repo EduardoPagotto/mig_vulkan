2048e806291d84844db88d86b3accfa88250f034
Edu Pagotto <edupagotto@gmail.com>
2026-06-25 23:36:47 -0300

    render reorg

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -18,52 +18,48 @@ namespace ce {
 
         // ATTACHEMNTS
         // Colour attachment of render pass
-        VkAttachmentDescription colourAttachemnt = {};
-        colourAttachemnt.format = this->swc->getSwapchainImageFormat();     // Format to use for attachment
-        colourAttachemnt.samples = VK_SAMPLE_COUNT_1_BIT;                   // Number of samples to write for multisampling
-        colourAttachemnt.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;              // Describes what to do with attachemnt before rendering
-        colourAttachemnt.storeOp = VK_ATTACHMENT_STORE_OP_STORE;            // Describes what todo with attachment after rendering
-        colourAttachemnt.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;   // Describes what todo with stencil before rendering
-        colourAttachemnt.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE; // Describes what todo with stencil before rendering
-
         // Framebuffer data will be storage as an image, but images can be given different data layouts
         // to give optimal use for certan operations
-        colourAttachemnt.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;     // Image data layout before render pass starts
-        colourAttachemnt.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR; // Imagedata layout after render pass (to change to)
+        const VkAttachmentDescription colourAttachemnt{
+            .format = this->swc->getSwapchainImageFormat(),     // Format to use for attachment
+            .samples = VK_SAMPLE_COUNT_1_BIT,                   // Number of samples to write for multisampling
+            .loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR,              // Describes what to do with attachemnt before rendering
+            .storeOp = VK_ATTACHMENT_STORE_OP_STORE,            // Describes what todo with attachment after rendering
+            .stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE,   // Describes what todo with stencil before rendering
+            .stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE, // Describes what todo with stencil before rendering
+            .initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,         // Image data layout before render pass starts
+            .finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR,     // Image data layout after render pass (to change to)
 
-        // depth attachemnt of render pass
-        VkAttachmentDescription depthAttachemnt = {};
-        depthAttachemnt.format = ChooseSupportedFormat(
-            this->vwrapp->getPhysical(), {VK_FORMAT_D32_SFLOAT_S8_UINT, VK_FORMAT_D32_SFLOAT, VK_FORMAT_D24_UNORM_S8_UINT}, // Formats
-            VK_IMAGE_TILING_OPTIMAL,                                                                                        // Tilling
-            VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT);
+        };
 
-        depthAttachemnt.samples = VK_SAMPLE_COUNT_1_BIT;
-        depthAttachemnt.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-        depthAttachemnt.storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-        depthAttachemnt.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
-        depthAttachemnt.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-        depthAttachemnt.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-        depthAttachemnt.finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+        // Depth attachemnt of render pass
+        const VkAttachmentDescription depthAttachemnt{
+            .format = ChooseSupportedFormat(this->vwrapp->getPhysical(),
+                                            {VK_FORMAT_D32_SFLOAT_S8_UINT, VK_FORMAT_D32_SFLOAT, VK_FORMAT_D24_UNORM_S8_UINT}, // Formats
+                                            VK_IMAGE_TILING_OPTIMAL,                                                           // Tilling
+                                            VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT),
+
+            .samples = VK_SAMPLE_COUNT_1_BIT,
+            .loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR,
+            .storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE,
+            .stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE,
+            .stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE,
+            .initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+            .finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL};
 
         // REFERENCES
         // Attachemnt reference uses an attachemnt index that refer to index in the attachemnt list passes to
         // renderPassCreateInfo
-        VkAttachmentReference colourAttachmentReference = {};
-        colourAttachmentReference.attachment = 0;
-        colourAttachmentReference.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+        const VkAttachmentReference colourAttachmentReference{.attachment = 0, .layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL};
 
         // Depth Attachment Refence
-        VkAttachmentReference depthAttachemntReference = {};
-        depthAttachemntReference.attachment = 1;
-        depthAttachemntReference.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+        const VkAttachmentReference depthAttachemntReference{.attachment = 1, .layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL};
 
-        // Information abaout a particular subpass the render pass is using
-        VkSubpassDescription subpass = {};
-        subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS; // Pipeline type subpass is to be bound to
-        subpass.colorAttachmentCount = 1;
-        subpass.pColorAttachments = &colourAttachmentReference;
-        subpass.pDepthStencilAttachment = &depthAttachemntReference;
+        // Information about a particular subpass the render pass is using
+        const VkSubpassDescription subpass{.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS, // Pipeline type subpass is to be bound to
+                                           .colorAttachmentCount = 1,
+                                           .pColorAttachments = &colourAttachmentReference,
+                                           .pDepthStencilAttachment = &depthAttachemntReference};
 
         // Need to determine when layout transitions occour subpass dependencies
         std::array<VkSubpassDependency, 2> subpassDependencies;
@@ -98,18 +94,15 @@ namespace ce {
         std::array<VkAttachmentDescription, 2> renderPassAttachemnts = {colourAttachemnt, depthAttachemnt};
 
         // Create Info for render pass
-        VkRenderPassCreateInfo renderPassCreateInfo = {};
-        renderPassCreateInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
-        renderPassCreateInfo.attachmentCount = static_cast<uint32_t>(renderPassAttachemnts.size());
-        renderPassCreateInfo.pAttachments = renderPassAttachemnts.data();
-        renderPassCreateInfo.subpassCount = 1;
-        renderPassCreateInfo.pSubpasses = &subpass;
-        renderPassCreateInfo.dependencyCount = static_cast<uint32_t>(subpassDependencies.size());
-        renderPassCreateInfo.pDependencies = subpassDependencies.data();
+        const VkRenderPassCreateInfo renderPassCreateInfo{.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO,
+                                                          .attachmentCount = static_cast<uint32_t>(renderPassAttachemnts.size()),
+                                                          .pAttachments = renderPassAttachemnts.data(),
+                                                          .subpassCount = 1,
+                                                          .pSubpasses = &subpass,
+                                                          .dependencyCount = static_cast<uint32_t>(subpassDependencies.size()),
+                                                          .pDependencies = subpassDependencies.data()};
 
-        VkResult result = vkCreateRenderPass(vwrapp->getLogical(), &renderPassCreateInfo, nullptr, &this->renderPass);
-
-        if (result != VK_SUCCESS) {
+        if (vkCreateRenderPass(vwrapp->getLogical(), &renderPassCreateInfo, nullptr, &this->renderPass) != VK_SUCCESS) {
             throw std::runtime_error("Failed to create render pass!!!");
         }
     }
