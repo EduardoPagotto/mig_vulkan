997ca27b68c4d00de97671d852067b7a1c1acf2c
Edu Pagotto <edupagotto@gmail.com>
2026-06-27 00:35:33 -0300

    Command buffer desenho

--- include/Command.hpp
+++ include/Command.hpp
@@ -8,7 +8,7 @@ namespace ce {
     class CommandPool {
       public:
         explicit CommandPool(VkPhysicalDevice physicalDevice, VkDevice logicalDevice, VkSurfaceKHR surface)
-            : physicalDevice(physicalDevice), logicalDevice(logicalDevice), surface(surface) {
+            : logicalDevice(logicalDevice), surface(surface) {
 
             // Get inidices of queue families from device
             ce::QueueFamilyIndices queueFamilyIndices = GetQueueFamilies(physicalDevice, this->surface);
@@ -34,9 +34,78 @@ namespace ce {
         VkCommandPool& getCommandPool() { return this->commandPool; }
 
       private:
-        VkPhysicalDevice physicalDevice;
+        // VkPhysicalDevice physicalDevice;
         VkDevice logicalDevice;
         VkCommandPool commandPool; // graphicsCommandPool;
         VkSurfaceKHR surface;
     };
+
+    class CommandBuffer {
+
+        explicit CommandBuffer(VkDevice device, VkCommandPool commandPool, size_t count) : device(device), commandPool(commandPool) {
+
+            this->commandBuffers.resize(count);
+
+            const VkCommandBufferAllocateInfo cbAllocInfo{
+                .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
+                .commandPool = commandPool,
+                .level = VK_COMMAND_BUFFER_LEVEL_PRIMARY, // VK_COMMAND_BUFFER_LEVEL_PRIMARY : Buffer you submit directly
+                                                          // to queue. Can't be called by other buffers.
+                                                          // VK_COMMAND_BUFFER_LEVEL_SECUNDARY : Buffer can't be called
+                                                          // directly. cam be called from other buffe via
+                                                          // "VkCmdExecuteCommand" when recording commands in primary buf
+                .commandBufferCount = static_cast<uint32_t>(commandBuffers.size())};
+
+            // Allocate command buffers and place handles in array of buffers
+            if (vkAllocateCommandBuffers(device, &cbAllocInfo, this->commandBuffers.data()) != VK_SUCCESS) {
+                throw std::runtime_error("Failed to Allocate Command buffers!");
+            }
+        }
+
+      private:
+        void beginCommandBuffer(size_t index, VkCommandBufferUsageFlagBits flag) {
+
+            // Information to begin the command buffer record
+            const VkCommandBufferBeginInfo beginInfo{
+                .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
+                .flags = flag // We're only using the command buffer once, so set up for one time submit
+            };
+
+            // Begin recording transfer commands
+            if (vkBeginCommandBuffer(commandBuffers[index], &beginInfo) != VK_SUCCESS) {
+                throw std::runtime_error("Failed to start recording a Command Buffer!");
+            }
+        }
+
+        void endCommandBuffer(size_t index) {
+            // End commands
+            if (vkEndCommandBuffer(this->commandBuffers[index]) != VK_SUCCESS) {
+                throw std::runtime_error("Failed to stop recording a Command Buffer!");
+            }
+        }
+
+        void submit(VkQueue queue) {
+            // Queue submission information
+            const VkSubmitInfo submitInfo{
+                .sType = VK_STRUCTURE_TYPE_SUBMIT_INFO,                                   //
+                .commandBufferCount = static_cast<uint32_t>(this->commandBuffers.size()), //
+                .pCommandBuffers = this->commandBuffers.data()                            //
+            };
+
+            // Submit transfer command to transfer queue and wait until it finishes
+            if (vkQueueSubmit(queue, 1, &submitInfo, VK_NULL_HANDLE) != VK_SUCCESS) {
+                throw std::runtime_error("Failed to submit Command Buffer to Queue!");
+            }
+
+            vkQueueWaitIdle(queue);
+
+            // Free temporary command buffer back to pool
+            vkFreeCommandBuffers(this->device, this->commandPool, this->commandBuffers.size(), this->commandBuffers.data());
+        }
+
+        VkDevice device;
+        VkCommandPool commandPool;
+        std::vector<VkCommandBuffer> commandBuffers;
+    };
+
 } // namespace ce
