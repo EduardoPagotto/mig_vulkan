5187589f3de3c5e1e92ef0a728dd46a8b07aae3b
Edu Pagotto <edupagotto@gmail.com>
2026-07-02 20:55:59 -0300

    Sync of Render in BufferCommand working

--- cevk/include/buffers/CommandBuffer.hpp
+++ cevk/include/buffers/CommandBuffer.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <array>
-#include <stdexcept>
 #include <vector>
 #include <vulkan/vulkan_core.h>
 
@@ -31,29 +29,7 @@ namespace ce {
 
         std::vector<VkCommandBuffer>& getBuffers() { return this->commandBuffers; }
 
-        void submitToRender(const SubmitToRenderInfo& sub) {
-            // -- SUBMIT COMMAND BUFFER TO RENDER
-            // Queue submission information
-            std::array<VkSemaphore, 1> waitSemaphores{sub.wait};                    //{this->imageAvailable[this->currentFrame]};
-            std::array<VkSemaphore, 1> signalSemaphores{sub.signal};                //{this->renderFinished[this->currentFrame]};
-            std::array<VkPipelineStageFlags, 1> waitStages{sub.pipelineStageFlags}; //{VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT};
-
-            const VkSubmitInfo submitInfo{
-                .sType = VK_STRUCTURE_TYPE_SUBMIT_INFO,
-                .waitSemaphoreCount = static_cast<uint32_t>(waitSemaphores.size()), // Number of semaphores to wait on
-                .pWaitSemaphores = waitSemaphores.data(),                           //
-                .pWaitDstStageMask = waitStages.data(),                             // Stagegs to check semaphores at
-                .commandBufferCount = 1,                                   // Number of command buffers to submit FIXME: é isto mesmo?
-                .pCommandBuffers = &this->commandBuffers[sub.bufferIndex], // Command buffer to submit
-                .signalSemaphoreCount = static_cast<uint32_t>(signalSemaphores.size()), // Number of semaphore to signal
-                .pSignalSemaphores = signalSemaphores.data(),                           // Semaphore to signal when command buffer finishes
-            };
-
-            // Submit command buffer to queue
-            if (vkQueueSubmit(sub.gQueue, 1, &submitInfo, sub.fence) != VK_SUCCESS) {
-                throw std::runtime_error("Failed to submit Command Buffer to Queue!");
-            }
-        }
+        void submitToRender(const SubmitToRenderInfo& sub);
 
       private:
         VkDevice device;
--- cevk/src/buffers/CommandBuffer.cpp
+++ cevk/src/buffers/CommandBuffer.cpp
@@ -1,4 +1,5 @@
 #include "buffers/CommandBuffer.hpp"
+#include <array>
 #include <stdexcept>
 
 namespace ce {
@@ -61,6 +62,30 @@ namespace ce {
         // End commands
         if (vkEndCommandBuffer(this->commandBuffers[index]) != VK_SUCCESS) {
             throw std::runtime_error("Failed to end a Command Buffer!");
+        }
+    }
+
+    void CommandBuffer::submitToRender(const SubmitToRenderInfo& sub) {
+        // -- SUBMIT COMMAND BUFFER TO RENDER
+        // Queue submission information
+        std::array<VkSemaphore, 1> waitSemaphores{sub.wait};                    //{this->imageAvailable[this->currentFrame]};
+        std::array<VkSemaphore, 1> signalSemaphores{sub.signal};                //{this->renderFinished[this->currentFrame]};
+        std::array<VkPipelineStageFlags, 1> waitStages{sub.pipelineStageFlags}; //{VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT};
+
+        const VkSubmitInfo submitInfo{
+            .sType = VK_STRUCTURE_TYPE_SUBMIT_INFO,
+            .waitSemaphoreCount = static_cast<uint32_t>(waitSemaphores.size()), // Number of semaphores to wait on
+            .pWaitSemaphores = waitSemaphores.data(),                           //
+            .pWaitDstStageMask = waitStages.data(),                             // Stagegs to check semaphores at
+            .commandBufferCount = 1,                                            // Number of command buffers to submit FIXME: é isto mesmo?
+            .pCommandBuffers = &this->commandBuffers[sub.bufferIndex],          // Command buffer to submit
+            .signalSemaphoreCount = static_cast<uint32_t>(signalSemaphores.size()), // Number of semaphore to signal
+            .pSignalSemaphores = signalSemaphores.data(),                           // Semaphore to signal when command buffer finishes
+        };
+
+        // Submit command buffer to queue
+        if (vkQueueSubmit(sub.gQueue, 1, &submitInfo, sub.fence) != VK_SUCCESS) {
+            throw std::runtime_error("Failed to submit Command Buffer to Queue!");
         }
     }
 
